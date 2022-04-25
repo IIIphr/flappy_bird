074a872d92047ab865027529814ee5243b662501
Sepehr <62986692+IIIphr@users.noreply.github.com>
2022-04-25 21:52:48 +0430

    Floor and roof are here, animated :D

---
+++ Assets/Scripts/lava_animation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lava_animation : MonoBehaviour
+{
+    [SerializeField] private float animation_speed = 0.01f;
+    private Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        renderer.material.mainTextureOffset = new Vector2(
+            (renderer.material.mainTextureOffset.x - animation_speed) % 1,
+            0);
+    }
+}
