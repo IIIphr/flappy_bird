4cd0743c2d26b48ddd49d3263356de5fddfe9ce0
Sepehr <62986692+IIIphr@users.noreply.github.com>
2022-04-25 22:08:07 +0430

    Adding Bird logics...

---
+++ Assets/Scripts/bird_controller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird_controller : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float jump_force = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            rb.AddForce(new Vector3(0, jump_force, 0));
+        }
+    }
+}
